d2c5f6765cc6e951a872015ac759718b5fec8172
mustafa <mstfssk@gmail.com>
2014-11-17 04:57:07 +0200

    adding todos

--- MyToDos.py
+++ MyToDos.py
@@ -1,17 +1,22 @@
 #I started to write it now.. 15.11.2014 --- 04:36
 #coding: utf-8
 ToDoList = []
-todo = {}
-
-fileObject = open("Todos.txt", "a")
 
 
 def createTodo():
+    completed = "Not completed"
     todo = raw_input("Write your todo:")
     precedence = raw_input("write precedence: ")
-    subtodo = raw_input("Add a subtodo: ")
-    todo = {"todo": todo, "precedence": precedence, "subtodo": subtodo}
+    todo = {"todo": todo, "precedence": precedence, "Completed": completed}
     return todo
+
+
+def save(ToDos):
+    for todo in ToDos:
+        fileObject = open("Todos.txt", "a")
+        fileObject.write("%s %s %s\n" %(todo["todo"], todo["precedence"], todo["Completed"]))
+        fileObject.close()
+
 
 if __name__ == "__main__":
     command = ""
@@ -21,9 +26,10 @@ if __name__ == "__main__":
             todo = createTodo()
             ToDoList.append(todo)
             print "new todo added"
-            print todo
+            print todo["todo"], todo["precedence"], todo["Completed"]
         elif command == "delete":
             print "delete :)"
     for todo in ToDoList:
         print todo["todo"]
+    save(ToDoList)
     print "saved all changes"
