6d1e67da3fcb11c445a2ba78de648302a6cbe4c8
Mustafa Şişik <mustafa.sisik@boun.edu.tr>
2014-11-21 23:51:45 +0200

    last edited

--- MyToDos.py
+++ MyToDos.py
@@ -1,99 +1,40 @@
-from sys import argv
-#coding: utf-8
-usage = """Usage:
-        1) In order to add todo first type -a and than type -command
-        such as -t, -s etc after the command type your text as string
-        (-t "I should go") these commands are:
-        -t, -d, -s, -dd, -p and you can create your commands.
+import argparse
+import csv
 
-        2) If you want to list all todo type -l and for detailed type -ld
-        3) you can remove a todo by typing -r index (-r 3)
-        4) -u 4 -s "Done" updates 4. todo's status
-        """
+parser = argparse.ArgumentParser(prog="MYTODO",
+                                 description="Todo Management Ssytem",
+                                 epilog="A new look to time management")
+parser.add_argument("-a", "--add", help="add new todo", action="store_true")
+parser.add_argument("-t", "--title", help="title of todo", type=str)
+parser.add_argument("-d", "--description",
+                    help="description of todo", type=str)
+parser.add_argument("-p", "--priority", help="priority of todo", type=str)
+parser.add_argument("-s", "--status", help="status of todo", type=str)
+parser.add_argument("--dd", help="duedate of todo", type=str)
+parser.add_argument("-l", help="lists all todos", action="store_true")
+parser.add_argument("-ld", help="lists details of all todos",
+                    action="store_true")
+parser.add_argument("-u", "--update", help="updates a todo by index",
+                    action="store_true")
+parser.add_argument("-r", help="remove a todo by index", action="store_true")
 
-
-def addToDo():
-    fileObject = open("todos.txt", "a")
-    for i in argv[2:]:
-        fileObject.write("%s " % i)
-    fileObject.write("\n")
-    fileObject.close()
-    print "New Todo Added"
-
-
-def readToDoFile():
-    todoList = []
-    fileObject = open("todos.txt", "r")
-    for line in fileObject:
-        line = line.lstrip("-").rstrip("\n")
-        elements = line.split("-")
-        todo = {}
-        for e in elements:
-            spaceIndex = e.index(" ")
-            command = e[:spaceIndex]
-            content = e[spaceIndex+1:-1]
-            todo[command] = content
-        todoList.append(todo)
-    return todoList
+a = parser.parse_args()
 
 
-def listToDos(todoList):
-    for todo in todoList:
-        print todoList.index(todo), todo["t"]
+def addTodo():
+    todo = {}
+    todo["title"] = a.title
+    todo["description"] = a.description
+    todo["priority"] = a.priority
+    todo["status"] = a.status
+    todo["duedate"] = a.dd
+    return todo
 
 
-def detailedList(todoList):
-    for todo in todoList:
-        print todoList.index(todo), "%s - %s" % (todo["t"], todo["p"])
-        print "description: %s" % todo["d"]
-
-
-def remove(index):
-    fileObject = open("todos.txt", "r")
-    lines = fileObject.readlines()
-    fileObject.close()
-    fileObject = open("todos.txt", "w")
-    lines.remove(lines[index])
-    for line in lines:
-        fileObject.write(line)
-    fileObject.close()
-    print "todo %d removed" % index
-
-
-def update(index, command, content):
-    fileObject = open("todos.txt", "r")
-    lines = fileObject.readlines()
-    fileObject.close()
-    line = lines[index]
-
-    indexStart = line.index(command)
-    indexEnd = indexStart + line[indexStart + 1:].find("-")
-    lineStartPart = line[:indexStart]
-    print lineStartPart
-    lineUpdatedPart = "%s %s " % (command, content)
-    print lineUpdatedPart
-    lineEndPart = line[indexEnd:]
-    print lineEndPart
-    newLine = "%s%s%s" % (lineStartPart, lineUpdatedPart, lineEndPart)
-    lines[index] = newLine
-    fileObject = open("todos.txt", "w")
-    for line in lines:
-        fileObject.write(line)
-    print "todo %s's %s is updated" % (index, command)
-
-
-def work():
-    if argv[1] == "-a":
-        addToDo()
-    elif argv[1] == "-l":
-        listToDos(readToDoFile())
-    elif argv[1] == "-ld":
-        detailedList(readToDoFile())
-    elif argv[1] == "-r":
-        remove(int(argv[2]))
-    elif argv[1] == "-u":
-        update(int(argv[2]), argv[3], argv[4])
-    else:
-        print usage
-
-work()
+#deneme
+if a.add:
+    print addTodo()
+    w = csv.writer(open("todo.csv", "w"))
+    for key, val in addTodo().items():
+        w.writerow([key, val])
+    print "all done"
